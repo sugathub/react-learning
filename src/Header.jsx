2de8d67348eaa26b71d396eb0abcc6db7cf81a24
sugat jmgade <jamgdesugat42@gmail.com>
2025-09-19 01:24:58 +0530

    Attempted to display normal value on window but failed

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,13 @@
 function Header(){
+ let num=0;
+    const count=()=>{
+       console.log(num);
+        return  num++;
+    }
 
     return(
         <>
+        <button onClick={count}>Please click{num}</button>
         <h1>This is header component</h1>
         </>
     )
