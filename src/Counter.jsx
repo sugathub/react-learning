949b31458a2cad5fa39cb075a1f391ace5d59f73
sugat jmgade <jamgdesugat42@gmail.com>
2025-09-19 20:14:18 +0530

    i entering text and showing on display by using onchage event and useState

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -10,9 +10,23 @@ const Counter = () => {
     const sub=()=>{
         return setCounter(counter-1);
     }
+
+
+
+
+    const [name, setName] =useState("");
+    const nameChange=(event)=>{
+         setName(event.target.value);
+    }
+
+
+
   return (
     <><h1>{counter}</h1>
     <button onClick={add}>+</button><button onClick={sub}>-</button>
+
+    <h1>{name}</h1>
+    <input type="text" value={name}   onChange={nameChange} placeholder='Enter Name' />
     
     </>
   )
