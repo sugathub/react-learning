847ef71fe64907dfa6a55f412b91cfcbfcc91067
sugat jmgade <jamgdesugat42@gmail.com>
2025-09-20 03:30:17 +0530

    lean how to toggle the component

--- src/Hidd.jsx
+++ src/Hidd.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Toggle } from './Toggle'
 
 export const Hidd = () => {
-  return (
-    <>
 
-    <Toggle />
-    </>
-  )
-}
+    const [dispaly, setDisplay] = useState(true);
+    const dis = () => {
+        setDisplay(!dispaly);
+    }
+
+
+    const [name, setName] = useState("Clear all");
+
+    const changeName = () => {
+        if (name == "Clear all") {
+            setName("Veiw All");
+
+        } else {
+            setName("Clear all");
+        }
+    }
+        return (
+            <>
+
+               {dispaly?<Toggle />:null}
+                <button onClick={() => { dis(), changeName() }}>{name}</button>
+            </>
+        )
+    }
