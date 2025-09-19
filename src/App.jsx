2d71e0df63ab7c3c18ec7b77870a044d636fb6e4
sugat jmgade <jamgdesugat42@gmail.com>
2025-09-20 02:44:46 +0530

    craeet on of button teax

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-     <h1>sugat </h1>
+     {/* <h1>sugat </h1> */}
 
 <Toggle />
 
--- src/Toggle.jsx
+++ src/Toggle.jsx
@@ -1,25 +1,19 @@
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 
 export const Toggle = () => {
-
-    const [display,setDisplay]=useState(true);
-const hid =()=>{
-    setDisplay(false);
-}
-const show =()=>{
-    setDisplay(true);
-}
-
+  const [display, setDisplay] = useState(true);
+  const hid = () => {
+    if (display == true) {
+      setDisplay(false);
+    } else {
+      setDisplay(true);
+    }
+  };
   return (
     <>
-
-    <button onClick={hid} onDoubleClick={show}>visible</button>
-
-    {
-    display?<h1>this is me</h1>:<h1>this is you</h1>
-}
-    
+      {display ? <h1>this is me</h1> : null}
+      <button onClick={hid}>visible</button>
     </>
-  )
-}
+  );
+};
