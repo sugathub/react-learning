01784008cc5a9d0c8cdf20f5228cbf356b1ad70c
sugat jmgade <jamgdesugat42@gmail.com>
2025-10-03 14:26:53 +0530

    Fix JSX map rendering by wrapping expression in curly braces

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,48 @@
 
 import './App.css'
-import { Table } from './Table'
+import User from './User'
+
 
 
 
 function App() {
 
+  const student = [
+    {
+      id: 1,
+     name: "sugat",
+      age: 66,
+
+
+    },
+    {
+      id: 2,
+       name: "kaka",
+      age: 67,
+
+    },
+    {
+      id: 3,
+      name: "kaku",
+      age: 54,
+
+    }
+  ]
+
   return (
     <>
-  
-<Table />
-     
+
+{
+
+student.map((user) =>(
+      <div key={user.id}>
+        <User name={user} age={user.age} id={user.id} />
+      </div>
+      ))
+
+}
+
+
     </>
   )
 }
