0c7623a2a2ae204a0c4b00b8afd7e634216cf1e4
sugat jmgade <jamgdesugat42@gmail.com>
2025-10-03 14:50:43 +0530

    inline css apply in  div

--- src/User.jsx
+++ src/User.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 
 const User = ({name,age,id}) => {
   return (
-    <>{name.name}
-  <br />
-    {age}
- <br />
-    {id}
+    <>
+    <div style={ {
+    backgroundColor: "yellowgreen",
+    border: "solid brown",
+}}> <h3>name : <span style={{color: 'blue'}}>{name.name}</span></h3>
+   
+    <h3>Age : <span style={{color: 'blue'}}>{age}</span></h3>
+   
+    <h3>id : <span style={{color: 'blue'}}>{id}</span></h3>
+  
+  </div>
+   
+    
 
     </>
   )
