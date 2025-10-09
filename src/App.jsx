fecb9b380a1d601a12f86788f251fa0d0bb9a524
sugat jmgade <jamgdesugat42@gmail.com>
2025-10-09 15:04:38 +0530

    i create object of style

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,12 @@
 
 function App() {
 
-  
+  const cardStyle={ border: "2px solid red",
+    width: "196px",
+    boxShadow: "1px 1px 1px 1px #34cecf",
+    height: "322px",
+
+  }
 
 
   return (
@@ -20,10 +25,7 @@ function App() {
 <h1>inline css</h1>
 
 
-<div style={{    border: "2px solid red",
-    width: "196px",
-    boxShadow: "1px 1px 1px 1px #34cecf",
-    height: "322px",}}>
+<div style={cardStyle}>
 
 
   <img src="https://www.bing.com/th/id/OIP.YBwCYCzeUKyYHPLdP3He2AHaDt?w=174&h=211&c=8&rs=1&qlt=90&o=6&cb=12&dpr=1.3&pid=3.1&rm=2" alt="girl" />
