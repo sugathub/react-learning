bf8e97fa971f7f119b34aca9f402faf3ebb82b10
sugat jmgade <jamgdesugat42@gmail.com>
2025-10-09 15:01:11 +0530

    i use inline css in card

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 
 // import './App.css'
 
-import { Collage } from "./Collage";
+
+
+
 
 
 
@@ -9,51 +11,25 @@ import { Collage } from "./Collage";
 
 function App() {
 
-  const collageData = [
-    {
-      name: "IIMA",
-      Rank: "428",
-      student: [
-       { name: "sugat"},
-       { age: "76"},
-      ]
-
-    },
-    {
-      name: "IIT",
-      Rank: "129",
-      student: [
-        {name: "JAY"},
-        {age: "66"},
-      ]
-
-    },
-    {
-      name: "IIMC",
-      Rank: "1448",
-      student: [
-        {name: "JAKE"},
-        {age: "34"},
-      ]
-
-    },
-
-  ];
+  
 
 
   return (
     <>
-   {
-    collageData.map((collage,index)=> (
+   
+<h1>inline css</h1>
 
-<div key={index}>
 
-<Collage collage={collage}/>
+<div style={{    border: "2px solid red",
+    width: "196px",
+    boxShadow: "1px 1px 1px 1px #34cecf",
+    height: "322px",}}>
+
+
+  <img src="https://www.bing.com/th/id/OIP.YBwCYCzeUKyYHPLdP3He2AHaDt?w=174&h=211&c=8&rs=1&qlt=90&o=6&cb=12&dpr=1.3&pid=3.1&rm=2" alt="girl" />
+  <h4>Neha</h4>
+  <p>software</p>
 </div>
-    ))
-}
-
-
 
     </>
   )
--- src/Student.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export const Student = ({student}) => {
-  return (
-    <div> <h3>Student</h3>
-<ul><li >Name :{student.name} - age{student.age} </li></ul></div>
-  )
-}
