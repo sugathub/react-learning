5da4a7acb4088200a7ce5f6392a0186dda85d6f1
sugat jmgade <jamgdesugat42@gmail.com>
2025-10-09 16:08:10 +0530

    create dynamics css in inline css

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,45 @@
 
 // import './App.css'
 
-
-
-
-
-
-
-
+import { useState } from "react"
 
 function App() {
 
-  const cardStyle={ border: "2px solid red",
+  const [cardStyle, setCardstyle] = useState({
+    border: "2px solid red",
     width: "196px",
     boxShadow: "1px 1px 1px 1px #34cecf",
     height: "322px",
 
+  });
+  const dark = () => {
+    setCardstyle({
+      ...cardStyle, backgroundColor: "black",
+      color: "#f7f7f7"
+    })
+  }
+  const white = () => {
+  setCardstyle({
+      ...cardStyle, backgroundColor: "#f7f7f7",
+      color: "#000000ff"
+    })
   }
 
 
   return (
     <>
-   
-<h1>inline css</h1>
+
+      <h1>inline css</h1>
+
+      <button onClick={dark}>Drake mode</button> <button onClick={white}>white mode</button>
+
+      <div style={cardStyle}>
 
 
-<div style={cardStyle}>
-
-
-  <img src="https://www.bing.com/th/id/OIP.YBwCYCzeUKyYHPLdP3He2AHaDt?w=174&h=211&c=8&rs=1&qlt=90&o=6&cb=12&dpr=1.3&pid=3.1&rm=2" alt="girl" />
-  <h4>Neha</h4>
-  <p>software</p>
-</div>
+        <img src="https://www.bing.com/th/id/OIP.YBwCYCzeUKyYHPLdP3He2AHaDt?w=174&h=211&c=8&rs=1&qlt=90&o=6&cb=12&dpr=1.3&pid=3.1&rm=2" alt="girl" />
+        <h4>Neha</h4>
+        <p>software</p>
+      </div>
 
     </>
   )
