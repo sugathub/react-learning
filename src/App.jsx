574a18c447d39c4f186aeaffa4b9db27fc99af03
sugat jmgade <jamgdesugat42@gmail.com>
2025-10-10 19:58:27 +0530

    instal react bootstap and import in main file use it

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,15 @@
 
 // import './App.css'
-import mu from './Style.module.css'
-import styled from "styled-components"
+
+import Navebar from "./Navebar"
 
 function App() {
 
 
-  const Hak = styled.h1`color: red;
-`;
-
+ 
   return (
     <>
-      <Hak>ok</Hak>
-      <h1>inline css</h1>
-
-      {/* <button onClick={dark}>Drake mode</button> <button onClick={white}>white mode</button> */}
-
-      <div className={mu.ok}>
-
-
-        <img src="https://www.bing.com/th/id/OIP.YBwCYCzeUKyYHPLdP3He2AHaDt?w=174&h=211&c=8&rs=1&qlt=90&o=6&cb=12&dpr=1.3&pid=3.1&rm=2" alt="girl" />
-        <h4>Neha</h4>
-        <p>software</p>
-      </div>
+     <Navebar />
 
     </>
   )
