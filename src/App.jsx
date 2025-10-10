ce525fb9c052731417a88123aef0355fbd79a059
sugat jmgade <jamgdesugat42@gmail.com>
2025-10-10 16:00:33 +0530

    create module.css file and import and use

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 // import './App.css'
-import './Style.css'
+import mu from './Style.module.css'
 
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
 
       {/* <button onClick={dark}>Drake mode</button> <button onClick={white}>white mode</button> */}
 
-      <div className="ok">
+      <div className={mu.ok}>
 
 
         <img src="https://www.bing.com/th/id/OIP.YBwCYCzeUKyYHPLdP3He2AHaDt?w=174&h=211&c=8&rs=1&qlt=90&o=6&cb=12&dpr=1.3&pid=3.1&rm=2" alt="girl" />
