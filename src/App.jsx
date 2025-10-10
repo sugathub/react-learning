8f6bee38008d162a7a882d24ad488f4ce1b0e1c0
sugat jmgade <jamgdesugat42@gmail.com>
2025-10-10 15:22:19 +0530

    create external CSS file and import

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,11 @@
 
 // import './App.css'
+import './Style.css'
 
-import { useState } from "react"
 
 function App() {
 
-  const [cardStyle, setCardstyle] = useState({
-    border: "2px solid red",
-    width: "196px",
-    boxShadow: "1px 1px 1px 1px #34cecf",
-    height: "322px",
-
-  });
-  const dark = () => {
-    setCardstyle({
-      ...cardStyle, backgroundColor: "black",
-      color: "#f7f7f7"
-    })
-  }
-  const white = () => {
-  setCardstyle({
-      ...cardStyle, backgroundColor: "#f7f7f7",
-      color: "#000000ff"
-    })
-  }
+ 
 
 
   return (
@@ -31,9 +13,9 @@ function App() {
 
       <h1>inline css</h1>
 
-      <button onClick={dark}>Drake mode</button> <button onClick={white}>white mode</button>
+      {/* <button onClick={dark}>Drake mode</button> <button onClick={white}>white mode</button> */}
 
-      <div style={cardStyle}>
+      <div className="ok">
 
 
         <img src="https://www.bing.com/th/id/OIP.YBwCYCzeUKyYHPLdP3He2AHaDt?w=174&h=211&c=8&rs=1&qlt=90&o=6&cb=12&dpr=1.3&pid=3.1&rm=2" alt="girl" />
