033b430710c506a353aecad985a6e7bca5b9f927
sugat jmgade <jamgdesugat42@gmail.com>
2025-10-12 12:59:26 +0530

    function pass as props

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
 
 // import './App.css'
 
+import { useState } from "react"
 import Navebar from "./Navebar"
+import { Counter } from "./Counter";
+
 
 function App() {
+
+ 
+const [counter,setCounter]=useState(0);
+const inc =()=>{
+  setCounter(counter+10);
+};
 
 
  
   return (
     <>
+
+
+
      <Navebar />
+
+     <h1>Counter {counter}</h1>
+    <Counter inc={inc} />
+     
+
+
+
 
     </>
   )
---
+++ src/Counter.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Counter = ({inc}) => {
+  return (
+   <>
+<button onClick={inc}>inc</button>
+  
+   </>
+  )
+}
