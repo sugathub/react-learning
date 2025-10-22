e7e82d8ce7979d4bf4f2fc34ac88fd0740156543
sugat jmgade <jamgdesugat42@gmail.com>
2025-10-22 15:30:26 +0530

    update an object using the ... spread operator

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
 // import './App.css'
 
 
+
 import { useState } from "react";
 import Navebar from "./Navebar";
-import { Usera } from "./Usera";
-import { Usera2 } from "./Usera2";
+
 
 function App() {
 
-  const [data, setData] = useState(" ");
+  const [data,setData]=useState({
+    name:"sugat",
+    age:22
+  })
+  const handalUpdate = (val)=>{
+    data.name =val;
+    console.log(val);
+    console.log(data.name);
+    setData({...data});
+
+  }
 
   return (
     <>
       <Navebar />
 
-      <Usera setData={setData} />
-      <br />
-      <hr />
-      <br />
-      <Usera2 data={data} />
-
+     <input type="text" placeholder="Update" onChange={()=>{handalUpdate(event.target.value)}} />
+     <br />
+     <hr />
+     <br />
+     <h4>Name: {data.name}</h4>
+     <p><b>{data.age}</b></p>
 
 
     </>
