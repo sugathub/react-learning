77a05782093ed6da7324dd4c60c9b3f27be1b10a
sugat jmgade <jamgdesugat42@gmail.com>
2025-10-23 15:08:42 +0530

    update the object

--- src/App.jsx
+++ src/App.jsx
@@ -10,26 +10,40 @@ function App() {
 
   const [data,setData]=useState({
     name:"sugat",
-    age:22
+    age:22,
+    as:{
+      ok:"sjjs",
+    }
   })
   const handalUpdate = (val)=>{
     data.name =val;
-    console.log(val);
-    console.log(data.name);
+   
     setData({...data});
 
   }
 
+
+  const handal = (val)=>{
+    data.as.ok =val;
+   
+    
+    setData({...data,as:{...data.as,val}});
+
+  }
   return (
     <>
       <Navebar />
 
      <input type="text" placeholder="Update" onChange={()=>{handalUpdate(event.target.value)}} />
+
+          <input type="text" placeholder="Update" onChange={()=>{handal(event.target.value)}} />
+
+
      <br />
      <hr />
      <br />
      <h4>Name: {data.name}</h4>
-     <p><b>{data.age}</b></p>
+     <p><b>{data.as.ok}</b></p>
 
 
     </>
