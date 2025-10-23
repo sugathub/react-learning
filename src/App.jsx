3a3e927f9cd7e92f41da446223ac59fde82bdce3
sugat jmgade <jamgdesugat42@gmail.com>
2025-10-23 20:18:17 +0530

    upade array

--- src/App.jsx
+++ src/App.jsx
@@ -2,49 +2,44 @@
 
 
 
+
 import { useState } from "react";
 import Navebar from "./Navebar";
 
 
 function App() {
 
-  const [data,setData]=useState({
-    name:"sugat",
-    age:22,
-    as:{
-      ok:"sjjs",
-    }
-  })
-  const handalUpdate = (val)=>{
-    data.name =val;
-   
-    setData({...data});
+  const [data,setData]=useState([
+    'sugat','jamgade','aditya','samyak'
 
-  }
+  ]);
 
 
-  const handal = (val)=>{
-    data.as.ok =val;
-   
-    
-    setData({...data,as:{...data.as,val}});
+const handal =(e)=>{
+  data[data.length-1]=e;
+  setData([...data]);
+}
 
-  }
+
   return (
     <>
       <Navebar />
 
-     <input type="text" placeholder="Update" onChange={()=>{handalUpdate(event.target.value)}} />
+      <input type="text" onChange={(e)=>{handal(e.target.value)}} placeholder="Update last element of array" />
 
-          <input type="text" placeholder="Update" onChange={()=>{handal(event.target.value)}} />
+      {
+        data.map((item,index)=>( <h3 key="index" >Name :{item}</h3>)
+         
+        )
+      }
 
 
-     <br />
-     <hr />
-     <br />
-     <h4>Name: {data.name}</h4>
-     <p><b>{data.as.ok}</b></p>
 
+
+
+
+
+    
 
     </>
   );
