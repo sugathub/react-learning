4b647563223c956fc17fc94dba42622b2a6b3ba0
sugat jmgade <jamgdesugat42@gmail.com>
2025-11-14 06:57:45 +0530

    Create a simple Validation by using REGEX(let regex =/^[A-z0-9]+$/i;     if (regex.test(event.target.value)) {}

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,52 @@
-import { Outlet, Route, Routes } from "react-router-dom";
-import "./app.css"
-import { Linkes } from "./Linkes";
-import { UserList } from "./UserList";
-import { AddUser } from "./AddUser";
-import { UserEdit } from "./UserEdit";
+import React, { useState } from 'react'
+import "./App.css"
+
 const App = () => {
 
+  const [name, setName] = useState();
+  const [nameErr, setNameErr] = useState();
+
+  const [password, setPassword] = useState();
+  const [passwordErr, setPasswordErr] = useState();
+
+
+  const handleName = (event) => {
+    if (event.target.value.length > 5) {
+      setNameErr("This is more then 5 charater")
+
+    } else {
+      setNameErr();
+      setName(event.target.value);
+    }
+  }
+
+  const handlePassword = (event) => {
+    let regex =/^[A-z0-9]+$/i;
+    if (regex.test(event.target.value)) {
+      setPasswordErr();
+
+    } else {
+      setPasswordErr("enter right password");
+
+    }
+  }
 
   return (
     <>
-
-    <Routes >
-      <Route element={<Linkes />}  >
-        
-
-        <Route path="/" element={<UserList />}/>
-        <Route path="/addUser" element={<AddUser />} />
-         <Route path="/edit/:id" element={<UserEdit />} />
-      </Route>
-       
-
-    </Routes>
-    
-
-    
-
+      <h2>Enter Informaction </h2>
+      <br />
+      <br />
+      <input className={nameErr ? 'namesty' : " "} type="text" onChange={handleName} placeholder='Enter Name' />
+      <span className='nameText' >{nameErr}</span>
+      <br />
+      <br />
+      <input type="text" className={passwordErr ? 'namesty' : " "} onChange ={handlePassword} placeholder='Enter passwords' />
+            <span className='nameText' >{passwordErr}</span>
+      <br />
+      <br />
+      <button disabled={passwordErr || nameErr }>Submit</button>
     </>
   )
 }
 
-export default App;
+export default App
